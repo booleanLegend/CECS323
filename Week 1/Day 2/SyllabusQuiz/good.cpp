30ab49ce99c571bdb96d085dfecb0488b14c2fe2
booleanLegend <78941300+booleanLegend@users.noreply.github.com>
2021-06-07 17:58:34 -0700

    Add files via upload
    Added all but DataGrip directory from OneDrive

---
+++ Week 1/Day 2/SyllabusQuiz/good.cpp
@@ -0,0 +1,48 @@
+
+Question 1		0.2 / 0.2 points
+If an assignment is hard to understand, it's best to keep that to yourself because Mr. Brown will throw a real rant if you ever question the clarity of his instructions.
+	True
+✅	False
+Question 2		0.2 / 0.2 points
+The reading is optional.
+	True
+✅	False
+Question 3		0.2 / 0.2 points
+The URL for the online text is: ___http://www.csulb.edu/colleges/coe/cecs/dbdesign/dbdesign.php___
+Question 4		0.2 / 0.2 points
+The points in this course are weighted.
+	True
+✅	False
+Question 5		0.2 / 0.2 points
+Piazza can be used for (check all that apply):
+Publishing your answers to see if anyone agrees with them.
+✅Asking about issues that you have in the course.
+✅Surveying the class to see whether anyone else is running in to the same problem that you are.
+✅Getting advice on the general approach to a problem.
+✅Getting clarification on an assignment.
+Question 6		0.2 / 0.2 points
+The final is comprehensive.
+	True
+✅	False
+Question 7		0.2 / 0.2 points
+Instead of midterm exams we will have:
+✅Quizzterms
+Quizzes
+Take home tests
+Quiznos
+Question 8		0.2 / 0.2 points
+There will be several 10 point lab exams.  Each one will be worth:
+5 points
+20 points
+none, these are strictly to see how well you are doing
+✅10 points
+Question 9		0.2 / 0.2 points
+Homework assignments can be turned in late, but for reduced credit.
+	True
+✅	False
+Question 10		0.2 / 0.2 points
+The final grade for this course is:
+Strict 60% D, 70% C, 80% B and 90% A.
+Standard bell curve.
+Grossly unfair.
+✅None of the above.
